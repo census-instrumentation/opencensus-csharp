0bb443b55c3225e1d9e674af1f3b0a8026866787
Sergey Kanzhelev <sergkanz@microsoft.com>
2018-07-30 09:45:30 -0700

    try CI

--- test/OpenCensus.Tests/Impl/Internal/TimestampConverterTest.cs
+++ test/OpenCensus.Tests/Impl/Internal/TimestampConverterTest.cs
@@ -22,7 +22,7 @@ namespace OpenCensus.Internal.Test
             mockClock.Setup(clock => clock.NowNanos).Returns(1234L);
 
             ITimestampConverter timeConverter = TimestampConverter.Now(mockClock.Object);
-            Assert.Equal(Timestamp.Create(1234, 10678), timeConverter.ConvertNanoTime(6234));
+            Assert.Equal(Timestamp.Create(1235, 10678), timeConverter.ConvertNanoTime(6234));
             Assert.Equal(Timestamp.Create(1234, 5444), timeConverter.ConvertNanoTime(1000));
             Assert.Equal(Timestamp.Create(1235, 0), timeConverter.ConvertNanoTime(999995556));
         }
