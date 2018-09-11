378831b93daad24038be7a70fef80875e07c507d
Sergey Kanzhelev <SergKanz@microsoft.com>
2018-09-10 22:50:33 -0700

    http client

---
+++ src/OpenCensus.Collector.Dependencies/Implementation/HttpClientListener.cs
@@ -0,0 +1,80 @@
+﻿// <copyright file="HttpClientListener.cs" company="OpenCensus Authors">
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of theLicense at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace OpenCensus.Collector.Dependencies.Implementation
+{
+    using System.Diagnostics;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using OpenCensus.Trace;
+    using OpenCensus.Trace.Sampler;
+
+    internal class HttpClientListener : ListenerHandler
+    {
+        private readonly PropertyFetcher startRequestFetcher = new PropertyFetcher("Request");
+        private readonly PropertyFetcher stopResponseFetcher = new PropertyFetcher("Response");
+        private readonly PropertyFetcher stopRequestStatusFetcher = new PropertyFetcher("RequestTaskStatus");
+
+        public HttpClientListener(ITracer tracer) : base("HttpHandlerDiagnosticListener", tracer)
+        {
+        }
+
+        public override void OnStartActivity(Activity activity, object payload)
+        {
+            var request = this.startRequestFetcher.Fetch(payload) as HttpRequestMessage;
+            if (request == null)
+            {
+                Debug.WriteLine("request is null");
+                return;
+            }
+
+            // TODO; sampling
+            this.LocalScope.Value = this.Tracer.SpanBuilder("HttpOut").SetSampler(Samplers.AlwaysSample).StartScopedSpan();
+            var span = this.Tracer.CurrentSpan;
+            // span.PutClientSpanKindAttribute();
+            // span.PutHttpMethodAttribute(request.Method.ToString());
+            // span.PutHttpHostAttribute(request.RequestUri.Host);
+            // span.PutHttpPathAttribute(request.RequestUri.AbsolutePath);
+            // span.PutHttpUrlAttribute(request.RequestUri.ToString());
+        }
+
+        public override void OnStopActivity(Activity activity, object payload)
+        {
+            var response = this.stopResponseFetcher.Fetch(payload) as HttpResponseMessage;
+            if (response == null)
+            {
+                Debug.WriteLine("response is null");
+                return;
+            }
+
+            var requestTaskStatus = this.stopRequestStatusFetcher.Fetch(payload) as TaskStatus?;
+
+            var span = this.Tracer.CurrentSpan;
+            if (requestTaskStatus.HasValue)
+            {
+                if (requestTaskStatus != TaskStatus.RanToCompletion)
+                {
+                    // span.PutErrorAttribute(requestTaskStatus.ToString());
+                }
+            }
+
+            // TODO status
+            // span.Status = new Status(((int)response.StatusCode >= 200 && (int)response.StatusCode < 300) ? CanonicalCode.Ok : CanonicalCode.Unknown, response.StatusCode.ToString());
+            // span.PutHttpStatusCodeAttribute((int)response.StatusCode);
+            this.LocalScope.Value?.Dispose();
+        }
+    }
+}
---
+++ src/OpenCensus.Collector.Dependencies/Implementation/ListenerHandler.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="ListenerHandler.cs" company="OpenCensus Authors">
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of theLicense at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace OpenCensus.Collector.Dependencies.Implementation
+{
+    using System.Diagnostics;
+    using System.Threading;
+    using OpenCensus.Common;
+    using OpenCensus.Trace;
+    using OpenCensus.Trace.Sampler;
+
+    internal class ListenerHandler
+    {
+        protected readonly ITracer Tracer;
+
+        // TODO: fix IScope and AsyncLocalContext so that current is always available
+        protected readonly AsyncLocal<IScope> LocalScope = new AsyncLocal<IScope>();
+
+        public ListenerHandler(string sourceName, ITracer tracer)
+        {
+            this.SourceName = sourceName;
+            this.Tracer = tracer;
+        }
+
+        public string SourceName { get; }
+
+        public virtual void OnStartActivity(Activity activity, object payload)
+        {
+            this.LocalScope.Value = this.Tracer.SpanBuilder(activity.OperationName).SetRecordEvents(true).SetSampler(Samplers.AlwaysSample).StartScopedSpan();
+        }
+
+        public virtual void OnStopActivity(Activity activity, object payload)
+        {
+            var span = this.Tracer.CurrentSpan;
+            foreach (var tag in activity.Tags)
+            {
+                // span.PutAttribute(tag.Key, AttributeValue.StringAttributeValue(tag.Value));
+            }
+
+            this.LocalScope.Value?.Dispose();
+        }
+    }
+}
