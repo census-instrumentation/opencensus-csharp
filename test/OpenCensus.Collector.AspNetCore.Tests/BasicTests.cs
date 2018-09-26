0e3cf25d2bd0f6454b120923cabe2d9db43a3182
Sergey Kanzhelev <sergkanz@microsoft.com>
2018-09-25 17:02:04 -0700

    WIP test project. It will break build

---
+++ test/OpenCensus.Collector.AspNetCore.Tests/BasicTests.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="BasicTests.cs" company="OpenCensus Authors">
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of theLicense at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace OpenCensus.Collector.AspNetCore.Tests
+{
+    using Xunit;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using TestApp.AspNetCore._2._0;
+    using System.Threading.Tasks;
+
+    // See https://github.com/aspnet/Docs/tree/master/aspnetcore/test/integration-tests/samples/2.x/IntegrationTestsSample
+    public class BasicTests
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> factory;
+
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Values")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = this.factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+    }
+}
