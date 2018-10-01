17fff2ab018fa840bc5edfdfecbfc965d994a6bc
Simon Zeltser <zeltser@google.com>
2018-10-01 16:16:20 +0000

    Adding support for links and capturing all types of spans (#50)
    * Introducing Stackdriver Exporter for Opencensus C# library
    
    - Current implementation can only store string values
    - Added the exporter and trace handler only
    - The exporter relies on newest Trace API from Stackdriver.
    
    * Updating translation from ISpan to Stackdriver's Span to cover more fields
    
    * Fixing the issue that prevented Stackdriver API call to succeed:
    now construction of Span resource is taken care of by SpanName class that
    is part of Stackdriver Trace V2 API.
    
    * - Added support for capturing all types of trace spans (long/bool/string)
    - Fixed csproj, so it produces both .NET Core and .NET versions. It also means signing the assembly using the same mechanism as other assemblies in the solution
    
    * - Added support for storing links
    - Minor fixes to proto<->opencensus translation methods
    
    * Fixing merge issue

--- src/OpenCensus.Exporter.Stackdriver/Utils/ProtoExtensions.cs
+++ src/OpenCensus.Exporter.Stackdriver/Utils/ProtoExtensions.cs
@@ -4,8 +4,17 @@ namespace OpenCensus.Exporter.Stackdriver.Utils
     using Google.Protobuf.WellKnownTypes;
     using OpenCensus.Common;
 
+    /// <summary>
+    /// Translation methods from Opencensus structures to common
+    /// Protobuf structures
+    /// </summary>
     public static class ProtoExtensions
     {
+        /// <summary>
+        /// Translates Opencensus Timestamp to Protobuf's timestamp
+        /// </summary>
+        /// <param name="timestamp">Opencensus timestamp</param>
+        /// <returns>Protobuf's timestamp</returns>
         public static Timestamp ToTimestamp(this ITimestamp timestamp)
         {
             return new Timestamp { Seconds = timestamp.Seconds, Nanos = timestamp.Nanos };
