7a999fcc195f24573ca02fd702d7b046a25e261e
Sergey Kanzhelev <SergKanz@microsoft.com>
2018-12-24 14:55:47 -0800

    added http mapping comments

--- src/OpenCensus.Abstractions/Trace/CanonicalCode.cs
+++ src/OpenCensus.Abstractions/Trace/CanonicalCode.cs
@@ -18,16 +18,21 @@ namespace OpenCensus.Trace
 {
     /// <summary>
     /// Canonical result code of span execution.
+    /// See https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto for definition.
     /// </summary>
     public enum CanonicalCode
     {
         /// <summary>
         /// The operation completed successfully.
+        ///
+        /// HTTP Mapping: 200 OK
         /// </summary>
         Ok = 0,
 
         /// <summary>
         /// The operation was cancelled (typically by the caller).
+        ///
+        /// HTTP Mapping: 499 Client Closed Request
         /// </summary>
         Cancelled = 1,
 
@@ -36,6 +41,8 @@ namespace OpenCensus.Trace
         /// from another address space belongs to an error-space that is not known in this address space.
         /// Also errors raised by APIs that do not return enough error information may be converted to
         /// this error.
+        ///
+        /// HTTP Mapping: 500 Internal Server Error
         /// </summary>
         Unknown = 2,
 
@@ -43,6 +50,8 @@ namespace OpenCensus.Trace
         /// Client specified an invalid argument. Note that this differs from FAILED_PRECONDITION.
         /// INVALID_ARGUMENT indicates arguments that are problematic regardless of the state of the
         /// system (e.g., a malformed file name).
+        ///
+        /// HTTP Mapping: 400 Bad Request
         /// </summary>
         InvalidArgument = 3,
 
@@ -51,16 +60,22 @@ namespace OpenCensus.Trace
         /// system, this error may be returned even if the operation has completed successfully. For
         /// example, a successful response from a server could have been delayed long enough for the
         /// deadline to expire.
+        ///
+        /// HTTP Mapping: 504 Gateway Timeout
         /// </summary>
         DeadlineExceeded = 4,
 
         /// <summary>
         /// Some requested entity (e.g., file or directory) was not found.
+        ///
+        /// HTTP Mapping: 404 Not Found
         /// </summary>
         NotFound = 5,
 
         /// <summary>
         /// Some entity that we attempted to create (e.g., file or directory) already exists.
+        ///
+        /// HTTP Mapping: 409 Conflict
         /// </summary>
         AlreadyExists = 6,
 
@@ -69,12 +84,16 @@ namespace OpenCensus.Trace
         /// must not be used for rejections caused by exhausting some resource (use RESOURCE_EXHAUSTED
         /// instead for those errors). PERMISSION_DENIED must not be used if the caller cannot be
         /// identified (use UNAUTHENTICATED instead for those errors).
+        ///
+        /// HTTP Mapping: 403 Forbidden
         /// </summary>
         PermissionDenied = 7,
 
         /// <summary>
         /// Some resource has been exhausted, perhaps a per-user quota, or perhaps the entire file system
         /// is out of space.
+        ///
+        /// HTTP Mapping: 429 Too Many Requests
         /// </summary>
         ResourceExhausted = 8,
 
@@ -89,12 +108,16 @@ namespace OpenCensus.Trace
         /// the system state has been explicitly fixed. E.g., if an "rmdir" fails because the directory
         /// is non-empty, FAILED_PRECONDITION should be returned since the client should not retry unless
         /// they have first fixed up the directory by deleting files from it.
+        ///
+        /// HTTP Mapping: 400 Bad Request
         /// </summary>
         FailedPrecondition = 9,
 
         /// <summary>
         /// The operation was aborted, typically due to a concurrency issue like sequencer check
         /// failures, transaction aborts, etc.
+        ///
+        /// HTTP Mapping: 409 Conflict
         /// </summary>
         Aborted = 10,
 
@@ -110,17 +133,23 @@ namespace OpenCensus.Trace
         /// using OUT_OF_RANGE (the more specific error) when it applies so that callers who are
         /// iterating through a space can easily look for an OUT_OF_RANGE error to detect when they are
         /// done.
+        ///
+        /// HTTP Mapping: 400 Bad Request
         /// </summary>
         OutOfRange = 11,
 
         /// <summary>
         /// Operation is not implemented or not supported/enabled in this service.
+        ///
+        /// HTTP Mapping: 501 Not Implemented
         /// </summary>
         Unimplemented = 12,
 
         /// <summary>
         /// Internal errors. Means some invariants expected by underlying system has been broken. If you
         /// see one of these errors, something is very broken.
+        ///
+        /// HTTP Mapping: 500 Internal Server Error
         /// </summary>
         Internal = 13,
 
@@ -129,16 +158,22 @@ namespace OpenCensus.Trace
         /// corrected by retrying with a backoff.
         ///
         /// See litmus test above for deciding between FAILED_PRECONDITION, ABORTED, and UNAVAILABLE.
+        ///
+        /// HTTP Mapping: 503 Service Unavailable
         /// </summary>
         Unavailable = 14,
 
         /// <summary>
         /// Unrecoverable data loss or corruption.
+        ///
+        /// HTTP Mapping: 500 Internal Server Error
         /// </summary>
         DataLoss = 15,
 
         /// <summary>
         /// The request does not have valid authentication credentials for the operation.
+        ///
+        /// HTTP Mapping: 401 Unauthorized
         /// </summary>
         Unauthenticated = 16,
     }
