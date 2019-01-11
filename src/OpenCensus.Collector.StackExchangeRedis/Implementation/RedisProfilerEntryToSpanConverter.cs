f3f5548c92c996b0d593e32680007dd12fa08812
Sergey Kanzhelev <sergkanz@microsoft.com>
2019-01-10 22:51:53 -0800

    add redis support

---
+++ src/OpenCensus.Collector.StackExchangeRedis/Implementation/RedisProfilerEntryToSpanConverter.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="RedisProfilerEntryToSpanConverter.cs" company="OpenCensus Authors">
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace OpenCensus.Collector.StackExchangeRedis.Implementation
+{
+    using OpenCensus.Trace;
+    using StackExchange.Redis.Profiling;
+
+    internal static class RedisProfilerEntryToSpanConverter
+    {
+        public static void DrainSession(ProfilingSession session, ITracer tracer)
+        {
+            foreach (var entry in session.FinishProfiling())
+            {
+                RecordSpan(entry, tracer);
+            }
+        }
+
+        public static void RecordSpan(IProfiledCommand command, ITracer tracer)
+        {
+            // use https://github.com/opentracing/specification/blob/master/semantic_conventions.md for now
+
+            var builder = tracer.SpanBuilder(command.Command); // Example: SET
+
+            var span = builder.StartSpan();
+
+            span.PutAttribute("db.statement", AttributeValue.StringAttributeValue(command.Command)); // Example: SET
+            span.PutAttribute("db.type", AttributeValue.StringAttributeValue("redis"));
+
+            span.End();
+
+            // db.instance
+            // command.Db; // 0
+
+            // command.EndPoint; // Unspecified/localhost:6379
+
+            // command.Flags; // None, DemandMaster
+
+            // command.CommandCreated; / /2019-01-10 22:18:28Z
+
+            // command.CreationToEnqueued; // 00:00:32.4571995
+            // command.ElapsedTime; // 00:00:32.4988020
+            // command.EnqueuedToSending; // 00:00:00.0352838
+            // command.ResponseToCompletion; // 00:00:00.0002601
+            // command.RetransmissionOf;
+            // command.RetransmissionReason;
+            // command.SentToResponse; // 00:00:00.0060586
+        }
+    }
+}
---
+++ src/OpenCensus.Collector.StackExchangeRedis/StackExchangeRedisCallsCollector.cs
@@ -0,0 +1,106 @@
+﻿// <copyright file="StackExchangeRedisCallsCollector.cs" company="OpenCensus Authors">
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace OpenCensus.Collector.StackExchangeRedis
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using OpenCensus.Collector.StackExchangeRedis.Implementation;
+    using OpenCensus.Trace;
+    using OpenCensus.Trace.Propagation;
+    using StackExchange.Redis.Profiling;
+
+    /// <summary>
+    /// Redis calls collector.
+    /// </summary>
+    public class StackExchangeRedisCallsCollector : IDisposable
+    {
+        private readonly ITracer tracer;
+
+        private readonly CancellationTokenSource cancellationTokenSource;
+        private readonly CancellationToken cancellationToken;
+
+        private readonly ProfilingSession defaultSession = new ProfilingSession();
+        private readonly ConcurrentDictionary<ISpan, ProfilingSession> cache = new ConcurrentDictionary<ISpan, ProfilingSession>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StackExchangeRedisCallsCollector"/> class.
+        /// </summary>
+        /// <param name="options">Configuration options for dependencies collector.</param>
+        /// <param name="tracer">Tracer to record traced with.</param>
+        /// <param name="sampler">Sampler to use to sample dependnecy calls.</param>
+        /// <param name="propagationComponent">Propagation component to use to encode span context to the wire.</param>
+        public StackExchangeRedisCallsCollector(StackExchangeRedisCallsCollectorOptions options, ITracer tracer, ISampler sampler, IPropagationComponent propagationComponent)
+        {
+            this.tracer = tracer;
+
+            this.cancellationTokenSource = new CancellationTokenSource();
+            this.cancellationToken = this.cancellationTokenSource.Token;
+            Task.Factory.StartNew(this.DumpEntries, TaskCreationOptions.LongRunning, this.cancellationToken);
+        }
+
+        /// <summary>
+        /// Returns session for the Redis calls recording.
+        /// </summary>
+        /// <returns>Session associated with the current span context to record Redis calls.</returns>
+        public Func<ProfilingSession> GetProfiler()
+        {
+            return () =>
+            {
+                var span = this.tracer.CurrentSpan;
+                if (span == null)
+                {
+                    return this.defaultSession;
+                }
+
+                var session = this.cache.GetOrAdd(span, (s) => new ProfilingSession(s));
+                return session;
+            };
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.cancellationTokenSource.Cancel();
+            this.cancellationTokenSource.Dispose();
+        }
+
+        private void DumpEntries(object state)
+        {
+            while (!this.cancellationToken.IsCancellationRequested)
+            {
+                RedisProfilerEntryToSpanConverter.DrainSession(this.defaultSession, this.tracer);
+
+                foreach (var entry in this.cache)
+                {
+                    var span = entry.Key;
+                    if (span.HasEnded)
+                    {
+                        this.cache.TryRemove(span, out var session);
+                        RedisProfilerEntryToSpanConverter.DrainSession(session, this.tracer);
+                    }
+                    else
+                    {
+                        this.cache.TryGetValue(span, out var session);
+                        RedisProfilerEntryToSpanConverter.DrainSession(session, this.tracer);
+                    }
+                }
+            }
+        }
+    }
+}
